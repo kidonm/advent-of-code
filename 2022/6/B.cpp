3b78ef867d20aba4d2415b23c0694a25c55f41e1
Marek Kidon <marek.kidon@gmail.com>
2022-12-06 18:49:41 +0100

    Optimize day 6B

--- 2022/6/B.cpp
+++ 2022/6/B.cpp
@@ -4,25 +4,40 @@ using namespace std;
 
 void solve()
 {
-    int k = 14;
+    unsigned int k = 14;
     int ans = -1;
     string s;
     cin >> s;
-    deque<char> as;
+    unordered_map<char, int> as;
+    deque<char> bs;
 
     for (unsigned int i = 0; i < s.size(); i++)
     {
         if (i < k)
         {
-            as.push_back(s[i]);
+            auto search = as.find(s[i]);
+            if (search == as.end())
+                as.insert({s[i], 1});
+            else
+                search->second++;
+
+            bs.push_back(s[i]);
             continue;
         }
 
-        as.pop_front();
-        as.push_back(s[i]);
+        auto search = as.find(bs.front());
+        if (--search->second == 0)
+            as.erase(search->first);
+        bs.pop_front();
+        bs.push_back(s[i]);
 
-        set as_set = set<char>(as.begin(), as.end());
-        if (as_set.size() == k)
+        search = as.find(bs.back());
+        if (search == as.end())
+            as.insert({bs.back(), 1});
+        else
+            search->second++;
+
+        if (as.size() == k)
         {
             ans = i;
             break;
