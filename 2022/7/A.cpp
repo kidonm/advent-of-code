5224fcbe958fb09abb21fa362d8227d21644452e
Marek Kidon <marek.kidon@gmail.com>
2022-12-07 23:53:44 +0100

    Day 7 A optimize to the dry bone more

--- 2022/7/A.cpp
+++ 2022/7/A.cpp
@@ -5,17 +5,18 @@ using namespace std;
 vector<string> input;
 unsigned int line = 0;
 long long ans = 0;
+unsigned int sz;
 
 long long walk()
 {
     long long curr = 0;
     int size;
-    while (line < input.size())
+    while (line < sz)
     {
         if (input[line][2] == 'l') // ls
         {
             line++;
-            while (line < input.size() && input[line][0] != '$')
+            while (line < sz && input[line][0] != '$')
             {
                 if (input[line][0] >= '0' && input[line][0] <= '9')
                 {
@@ -26,7 +27,7 @@ long long walk()
                 line++;
             }
         }
-        else if (input[line][2] == 'c') // cd
+        else // if (input[line][2] == 'c') // cd
         {
             if (input[line][5] == '.') // cd ..
             {
@@ -41,8 +42,8 @@ long long walk()
                 curr += walk();
             }
         }
-        else
-            assert(false && "Unknown command");
+        // else
+        //     assert(false && "Unknown command");
     }
 
     assert(false && "End");
@@ -52,7 +53,6 @@ void solve()
 {
     line++;
     walk();
-    cout << ans << '\n';
 }
 
 int main()
@@ -60,14 +60,16 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     freopen("a.in", "r", stdin);
-    freopen("a.out.txt", "w", stdout);
+    // freopen("a.out.txt", "w", stdout);
 
     string s;
     while (getline(cin, s))
         input.push_back(s);
+    sz = input.size();
 
     auto start = std::chrono::high_resolution_clock::now();
     solve();
     auto elapsed = std::chrono::high_resolution_clock::now() - start;
+    cout << ans << '\n';
     cerr << std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count() << '\n';
 }
