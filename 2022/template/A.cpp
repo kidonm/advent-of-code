6bd00ae6df31fe60933f0807ae4ce2cfd0759c78
Marek Kidon <marek.kidon@gmail.com>
2022-12-07 16:38:13 +0100

    Update template with chrono

--- 2022/template/A.cpp
+++ 2022/template/A.cpp
@@ -11,5 +11,8 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     freopen("a.in", "r", stdin);
+    auto start = std::chrono::high_resolution_clock::now();
     solve();
+    auto elapsed = std::chrono::high_resolution_clock::now() - start;
+    cout << std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count() << '\n';
 }
