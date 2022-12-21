cb05330942318b899f35efb0c849516a6d718620
Marek Kidon <marek.kidon@gmail.com>
2022-12-21 15:37:34 +0100

    Day 21

---
+++ 2022/21/A.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct operation
+{
+    int64_t val;
+
+    string left;
+    string right;
+    char op;
+};
+
+uint64_t eval(operation &node, map<string, operation> &ops)
+{
+
+    if (node.val == -1)
+    {
+        uint64_t eval_l = eval(ops[node.left], ops);
+        uint64_t eval_r = eval(ops[node.right], ops);
+        if (node.op == '+')
+            return eval_l + eval_r;
+        else if (node.op == '-')
+            return eval_l - eval_r;
+        else if (node.op == '*')
+            return eval_l * eval_r;
+        else if (node.op == '/')
+            return eval_l / eval_r;
+        else
+            assert(false && "unknown op");
+    }
+    else
+        return node.val;
+}
+
+void solve()
+{
+    string node;
+
+    map<string, operation> ops;
+
+    while (cin >> node)
+    {
+        char c;
+        cin >> c;
+        cin.putback(c);
+
+        operation op;
+        op.val = -1;
+        ops.insert({node, op});
+
+        if (c >= '0' && c <= '9')
+            cin >> ops[node].val;
+        else
+            cin >> ops[node].left >> ops[node].op >> ops[node].right;
+    }
+
+    // assert(ops["gcfc"].val == 6);
+    uint64_t ans = eval(ops["root"], ops);
+    cout << ans << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("a.in", "r", stdin);
+    // freopen("test.in", "r", stdin);
+    auto start = std::chrono::high_resolution_clock::now();
+    solve();
+    auto elapsed = std::chrono::high_resolution_clock::now() - start;
+    cout << std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count() << '\n';
+}
